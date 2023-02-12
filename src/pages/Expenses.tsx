e701a207dfce2639c1146b81b987122663429b03
Angelo Santos <axngrsantos@gmail.com>
2023-02-12 17:54:17 +0800

    Add General Styling for Expense page and hover styles for individual expenses

--- src/pages/Expenses.tsx
+++ src/pages/Expenses.tsx
@@ -1,5 +1,5 @@
 import './Expenses.css'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { ExpenseContext } from '../contexts/ExpenseContext'
 import { ExpenseObject } from '../contexts/ExpenseContext'
 import DeleteIcon from '../assets/delete.svg';
@@ -8,15 +8,38 @@ import Form from '../components/form/Form';
 
 const ExpenseData = ({id, title, recipient, allowance, description}: ExpenseObject) => {
 	const {deleteExpense} = useContext(ExpenseContext);
+
+	const [isHover, setIsHover] = useState(null);
+	const handleMouseEnter = (id:any) => {
+		setIsHover(id)
+	}
+
+	const handleMouseLeave = (id:number) => {
+		setIsHover(null)
+	}
+
+	const hoverStyle = {
+		boxShadow: "0 0 10px rgba(0,0,0,0.7)"
+	}
+
+	const IconHoverStyle = {
+		opacity:100
+	}
+
 	return(
-		<div className="expense">
+		<div 
+			className="expense" 
+			style={isHover === id ? hoverStyle : {}}
+			onMouseEnter={() => handleMouseEnter(id)} 
+			onMouseLeave={() => handleMouseLeave(id)}
+		>
 			<div className="expense-left-section">
 				<h2>{title}</h2>
 				<h3 id="recipient-name">From: {recipient}</h3>
 			</div>
 			<div className="expense-right-section">
 				<h1>{allowance}</h1>
-				<img src={DeleteIcon} alt="" onClick={(e) => deleteExpense(e, id)}/>
+				<img src={DeleteIcon} alt="" onClick={(e) => deleteExpense(e, id)} style={isHover === id ? IconHoverStyle : {opacity:0}}/>
 			</div>
 		</div>
 	)
@@ -24,7 +47,7 @@ const ExpenseData = ({id, title, recipient, allowance, description}: ExpenseObje
 
 const Expenses = () => {
 	const {expenses, showForm, toggleShowForm} = useContext(ExpenseContext);
-	console.log(expenses);
+
 
 	const expenseItems = expenses.map((expense) => {
 		return(
