d06ebb2bd4f0a2b2a746b997fc514a6a45c00128
Angelo Santos <axngrsantos@gmail.com>
2023-02-12 15:18:31 +0800

    Add add expense and delete expense function along with toggle function for forms

--- src/contexts/ExpenseContext.tsx
+++ src/contexts/ExpenseContext.tsx
@@ -17,7 +17,13 @@ export type ExpenseObject = {
 }
 
 type ExpenseContextType = {
-	expenses: ExpenseObject[]
+	expenses: ExpenseObject[],
+	expenseForm: ExpenseForm,
+	showForm:boolean,
+	toggleShowForm: (event:React.MouseEvent<HTMLButtonElement | HTMLDivElement>) => void
+	handleFormInputs: (event:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+	addExpense: (event:React.MouseEvent<HTMLButtonElement>) => void
+	deleteExpense: (event:React.MouseEvent<HTMLImageElement>, id:number) => void,
 }
 
 type ExpenseContextProviderProps = {
@@ -28,6 +34,8 @@ export const ExpenseContext = createContext({} as ExpenseContextType);
 
 export const ExpenseContextProvider = ({children}:ExpenseContextProviderProps) => {
 	const [expenses, setExpenses] = useState([])
+
+	const [showForm, setShowForm] = useState(false);
 	const [expenseForm, setExpenseForm] = useState<ExpenseForm>({title:'', recipient:'', allowance:0, description:''})
 
 
@@ -35,12 +43,48 @@ export const ExpenseContextProvider = ({children}:ExpenseContextProviderProps) =
 		loadAllExpenses();
 	},[])
 
+	const toggleShowForm = () => {
+		setShowForm(prevForm => {return !prevForm});
+	}
+
+	const handleFormInputs = (event:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		const {name, value} = event.target;
+		setExpenseForm(prevExpenseForm => {
+			return {...prevExpenseForm,
+				[name] : value
+			}
+		})
+	}
+
 	const loadAllExpenses = async () => {
 		const result = await axios.get("http://localhost:8080/api/expenses");
-		setExpenses(result.data);
+		if(result.data === ''){
+			setExpenses([])
+		}else{
+			setExpenses(result.data);
+		}
 	}
+
+	const addExpense = async () => {
+		await axios.post("http://localhost:8080/api/expenses", expenseForm)
+		loadAllExpenses();
+		setExpenseForm(
+			{
+				title:'', 
+				recipient:'', 
+				allowance:0, 
+				description:''
+			})
+		setShowForm(false);
+	}
+
+	const deleteExpense =  async (event:React.MouseEvent<HTMLImageElement>, id:number) => {
+		await axios.delete(`http://localhost:8080/api/expenses/${id}`)
+		loadAllExpenses();
+	}
+
 	return(
-		<ExpenseContext.Provider value={{expenses}} >
+		<ExpenseContext.Provider value={{expenses, expenseForm, showForm, toggleShowForm, handleFormInputs,addExpense, deleteExpense}} >
 			{children}
 		</ExpenseContext.Provider>
 	)
